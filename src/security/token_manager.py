c31c98d3ab0aa5862c4a69d5adc9a1161e350132
Oleksandr Semenenko <semenenko233249@gmail.com>
2025-08-05 22:28:05 +0300

    Added registration and account activation for user

--- src/security/token_manager.py
+++ src/security/token_manager.py
@@ -2,8 +2,6 @@ from datetime import datetime, timedelta, timezone
 from typing import Optional
 
 from jose import jwt, JWTError, ExpiredSignatureError
-
-from exceptions import TokenExpiredError, InvalidTokenError
 from security.interfaces import JWTAuthManagerInterface
 
 
